401ca139275b8dee55a4563493642c0ec92a1ef2
Nathan Oketch <nobuyanzi07@gmail.com>
2026-07-15 18:53:34 +0300

    completion of lab assignment

--- index.js
+++ index.js
@@ -4,3 +4,16 @@
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+require(`datejs`)
+
+function combineUsers(...userArrays) {
+  const users = [].concat(...userArrays)
+  const merge_date = Date.today().toString("M/d/yyyy")
+
+  return {users, merge_date}
+}
+
+module.exports = {
+  ...(typeof combineUsers !== `undefined`&& {combineUsers})
+}
